28adf6d057b52409d5c2355c2ca1dbca9f35da7b
HassanKhan123 <hk2602809@gmail.com>
2022-02-14 12:29:20 +0500

    user routes added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+} from '@nestjs/common';
 
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -10,5 +20,29 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    try {
+      return this.usersService.remove(parseInt(id));
+    } catch (error) {
+      throw error;
+    }
   }
 }
