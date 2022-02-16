8b5fcfa6b97f864b410444f5c0c77ef3fdbb1f99
HassanKhan123 <hk2602809@gmail.com>
2022-02-16 21:12:45 +0500

    custom interceptor and decorator added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Delete,
   NotFoundException,
   Session,
+  UseInterceptors,
 } from '@nestjs/common';
 
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -18,18 +19,27 @@ import { Serialize } from '../interceptors/serialize.interceptor';
 
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { User } from './user.entity';
 
 @Controller('auth')
 @Serialize(UserDto)
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
 
+  // @Get('/whoamI')
+  // async whoAmI(@Session() session: any) {
+  //   return this.usersService.findOne(session.userId);
+  // }
+
   @Get('/whoamI')
-  async whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  async whoAmI(@CurrentUser() user: User) {
+    return user;
   }
 
   @Post('/signout')
